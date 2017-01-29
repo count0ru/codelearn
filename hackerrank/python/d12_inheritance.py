31dee0e096360ecb3572d80f9e45b6c7d8613d6a
dna note <count0ru@gmail.com>
2017-01-29 23:10:38 +0300

    Quick commit from notebook

--- hackerrank/python/d12_inheritance.py
+++ hackerrank/python/d12_inheritance.py
@@ -1,37 +1,45 @@
 class Person:
     
     def __init__(self, firstName, lastName, idNumber):
+        
         self.firstName = firstName
         self.lastName = lastName
         self.idNumber = idNumber
+    
     def printPerson(self):
+        
         print("Name:", self.lastName + ",", self.firstName)
         print("ID:", self.idNumber)
 
 class Student(Person):
     
-    def __init_ (self, firstName, lastName, idNumbera, score):
+    def __init__(self, firstName, lastName, idNumber, score):
+        
         Person.__init__(self, firstName, lastName, idNumber)
-        self.score = score.split(" "))
-
+        self.score = score
 
     def calculate(self):
-        tests_amount = int(input().strip())
-        tests_results = int(input().split(" "))
-        scores_amount = sum(scores_amount)/len(scores_amount)
-        print(("O")[scores_amount in range(90,100)]) 
-        print(("E")[scores_amount in range(80,90)]) 
-        print(("A")[scores_amount in range(70,80)]) 
-        print(("P")[scores_amount in range(55,70)]) 
-        print(("D")[scores_amount in range(40,55)]) 
-        print(("T")[scores_amount in range(0,40)])
-
+        
+        scores_amount = sum(self.score)/len(self.score)
+        
+        if scores_amount in range(0,39): 
+            return("T")
+        elif scores_amount in range(41,54): 
+            return("D")
+        elif scores_amount in range(55,69): 
+            return("P")
+        elif scores_amount in range(70,79): 
+            return("A")
+        elif scores_amount in range(80,89): 
+            return("E")
+        elif scores_amount in range(90,100): 
+            return("O")
 
 line = input().split()
 firstName = line[0]
 lastName = line[1]
 idNum = line[2]
-#numScores = int(input()) # not needed for Python
+numScores = int(input()) # not needed for Python
 scores = list( map(int, input().split()) )
 s = Student(firstName, lastName, idNum, scores)
 s.printPerson()
