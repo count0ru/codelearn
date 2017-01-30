ac98e856afa7ece87b22d702565703660a81afa6
Dmitry Inlearno <dn@inlearno.ru>
2017-01-30 18:16:42 +0300

    Quick Commit

--- hackerrank/bash/comparing_numbers.sh
+++ hackerrank/bash/comparing_numbers.sh
@@ -1,9 +1,9 @@
 read X
 read Y
-if [[ $X > $Y ]]
+if  [ $X -gt $Y ]
         then echo "X is greater than Y"
-elif [[ $X == $Y ]]
+elif [ $X -eq $Y ]
         then echo "X is equal to Y"
-elif [[ $X < $Y ]]
+elif [ $X -lt $Y ]
         then echo "X is less than Y"
 fi
