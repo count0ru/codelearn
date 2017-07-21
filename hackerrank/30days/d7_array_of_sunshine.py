609537636c0c24789cb33279b895177350d71c82
Dmitry Nagovitsin <count0ru@gmail.com>
2017-07-21 16:47:55 +0300

    Update d7_array_of_sunshine.py

--- hackerrank/30days/d7_array_of_sunshine.py
+++ hackerrank/30days/d7_array_of_sunshine.py
@@ -1,5 +1,1 @@
-arr = [str(arr_temp) for arr_temp in input().strip().split(' ')]
-print(" ".join(reversed(arr)))
-
-
-
+print(" ".join(reversed([str(arr_temp) for arr_temp in input().strip().split(' ')])))
